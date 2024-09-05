7723919d7653dc6866f4b84842315125dffd0537
Christopher Moyano <cmoyano@ing.ucsc.cl>
2024-09-05 11:44:13 -0400

    Creacion de la ruleta y arreglo del fitness
    Se crea una ruleta para poder ir eligiendo la nueva población, además se arregla el fitnees para que el fitness mayor tenga mayor probabilidad y el menor menos probabilidad de salir

--- Nreinas.py
+++ Nreinas.py
@@ -9,7 +9,7 @@ def CrearPoblacion(n, p):
     return Poblacion
 
 def ContarFitness(Poblacion,n,p):
-    fitness= np.zeros(p,int)
+    fitness= np.zeros(p,float)
     for i in range(p):
         contador = 0
         for j in range(n):
@@ -18,8 +18,34 @@ def ContarFitness(Poblacion,n,p):
                     contador +=1
                 if((Poblacion[i][j]-j) == Poblacion[i][k]-k):
                     contador+=1
-        fitness[i] = contador
+        fitness[i] = (1/contador)
     return fitness
+
+def CrearRuleta(Fitness,p):
+    Ruleta = np.zeros(p,float)
+    proporcion = Fitness/Fitness.sum()
+    Ruleta[0]=proporcion[0]
+    for i in range (1,p):
+        Ruleta[i] = Ruleta[i-1]+proporcion[i]
+    return Ruleta
+
+def NuevaPoblacion(Poblacion,Ruleta,n,p):
+    i = 0
+    contador = 0
+    random = np.random.rand()
+    NuevaPoblacion = np.zeros((p,n),int)
+    individuo1 = np.zeros(n,int)
+    individuo2 = np.zeros(n,int)
+    while random<Ruleta[i] and contador<2:
+        if(random>Ruleta[i] and contador==0):
+            individuo1 = Poblacion[i]
+            contador+=1
+            i=0
+        elif(random>Ruleta[i] and contador==1)
+            individuo2=Poblacion[i]
+            contador+=1
+
+
 
 
 if len(sys.argv)==7:
@@ -41,6 +67,10 @@ Poblacion = CrearPoblacion(n,p)
 print(Poblacion)
 Fitness = ContarFitness(Poblacion,n,p)
 print(Fitness)
+Ruleta=CrearRuleta(Fitness,p)
+print(Ruleta)
+random = np.random.rand()
+print(random)
 
 
 
